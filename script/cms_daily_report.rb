558694051bc67698d1376358ba7b99e42bec38bd
Esme Rubinstein <51678367+erubinst@users.noreply.github.com>
2024-12-04 10:37:21 -0500

    adding metric for aptc csr plan selection count (#4791)
    * adding metric for aptc csr plan selection count
    
    * limiting to current day
    
    * fixing syntax error
    
    ---------
    
    Co-authored-by: Jacob Kagon <69021620+jacobkagon@users.noreply.github.com>

--- script/cms_daily_report.rb
+++ script/cms_daily_report.rb
@@ -7,6 +7,10 @@ def next_year
   @current_year + 1
 end
 
+prev_day = TimeKeeper.date_of_record.yesterday
+@state_at = TimeKeeper.start_of_exchange_day_from_utc(prev_day)
+@end_at = TimeKeeper.end_of_exchange_day_from_utc(prev_day)
+
 all_enrolled_people = HbxEnrollment.collection.aggregate([
   {"$match" => {
       "hbx_enrollment_members" => {"$ne" => nil},
@@ -43,7 +47,55 @@ all_people_ids = all_enrolled_people.map do |rec|
   rec["_id"]
 end
 
+csr_aptc_enrolled_people = HbxEnrollment.collection.aggregate([
+  {"$match" => {
+      "hbx_enrollment_members" => {"$ne" => nil},
+      "external_enrollment" => {"$ne" => true},
+      "coverage_kind" => "health",
+      "consumer_role_id" => {"$ne" => nil},
+      "product_id" => { "$ne" => nil },
+      "created_at" => {"$gte" => @state_at, "$lte" => @end_at},
+      "aasm_state" => {"$in" => HbxEnrollment::RENEWAL_STATUSES + HbxEnrollment::ENROLLED_STATUSES},
+      "effective_on" => {"$gte" => Date.new(next_year, 1, 1), "$lt" => Date.new(next_year + 1, 1, 1)}
+  }},
+  {"$lookup" => {
+    "from" => "benefit_markets_products_products",
+    "localField" => "product_id",
+    "foreignField" => "_id",
+    "as" => "product"
+  }},
+  {"$unwind" => "$product"},
+  {"$match" => {
+      "$or" => [
+        {"product.csr_variant_id" => {"$ne" => "01"}},
+        {"applied_aptc_amount.cents" => {"$gt" => 0}}
+      ]
+  }},
+  {"$project" => {
+      "family_id" => "$family_id",
+      "hbx_enrollment_members" => "$hbx_enrollment_members"
+  }},
+  {"$lookup" => {
+    "from" => "families",
+    "localField" => "family_id",
+    "foreignField" => "_id",
+    "as" => "family"
+  }},
+  {"$unwind" => "$family"},
+  {"$unwind" => "$family.family_members"},
+  {"$unwind" => "$hbx_enrollment_members"},
+  {"$project" => {
+      "family_id" => "$family_id",
+      "person_id" => "$family.family_members.person_id",
+      "applicant_id" => "$hbx_enrollment_members.applicant_id",
+      "person_and_member_match" => {"$eq" => ["$family.family_members._id", "$hbx_enrollment_members.applicant_id"]}
+  }},
+  {"$match" => {"person_and_member_match" => true}},
+  {"$group" => {"_id" => "$person_id", "total" => {"$sum" => 1}}}
+])
+
 all_enrolled_people_set = Set.new(all_people_ids)
+csr_aptc_enrolled_people_set = Set.new(csr_aptc_enrolled_people.map { |rec| rec["_id"] })
 
 pre_term_renewal_candidates = HbxEnrollment.collection.aggregate([
   {"$match" => {
@@ -251,6 +303,7 @@ active_renewals_set = (re_enrolled_member_set & post_11_1_purchase_set) - has_be
 passive_renewals_set = re_enrolled_member_set - (post_11_1_purchase_set - has_been_renewed_set)
 
 puts "Total Member Enrolled(#{next_year}) Count: #{all_enrolled_people_set.size}"
+puts "Number of plan selections with APTC or CSR on a single day: #{csr_aptc_enrolled_people_set.size}"
 puts "Total New Member/Consumer selected #{next_year} enrollments after 11/1/#{year} : #{new_member_set.size}"
 puts "Total Re-Enrolled(#{next_year}) Member: #{re_enrolled_member_set.size}"
 puts "Total Active Renewed(#{next_year}) Member: #{active_renewals_set.size}"
@@ -328,9 +381,6 @@ def process_users(users, offset_count)
   end
 end
 
-prev_day = TimeKeeper.date_of_record.yesterday
-@state_at = TimeKeeper.start_of_exchange_day_from_utc(prev_day)
-@end_at = TimeKeeper.end_of_exchange_day_from_utc(prev_day)
 users = User.all.where(:created_at => { "$gte" => @state_at, "$lte" => @end_at })
 total_user_count = users.count
 users_per_iteration = 10_000.0
@@ -405,10 +455,6 @@ end
 
 puts "6.1B Number of new Accounts created on a single day(No external app id). Total number of new accounts created yesterday: #{@total_new_families_count}"
 
-
-prev_day = TimeKeeper.date_of_record.yesterday
-@state_at = TimeKeeper.start_of_exchange_day_from_utc(prev_day)
-@end_at = TimeKeeper.end_of_exchange_day_from_utc(prev_day)
 @total_submitted_count = 0
 
 # Remove export of files containing PII
@@ -924,6 +970,7 @@ CSV.open("#{Rails.root}/CMS_daily_report_summary.csv", "w", force_quotes: true) 
       ["","CMS Reporting Summary",""],
       ["","",""],
       ["","Total Plan Selections (net)", all_enrolled_people_set.size],
+      ["", "Number of plan selections with APTC or CSR on a single day", csr_aptc_enrolled_people_set.size],
       ["","New Consumers (net)", new_member_set.size],
       ["","Total Re-enrollees (net)", re_enrolled_member_set.size],
       ["","Active Re-enrollees (net)", active_renewals_set.size],
